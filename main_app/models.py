472831ae58ae0468889655c27682e2dd29141566
kennytrinh <kenny.trinh4444@gmail.com>
2021-02-06 11:53:00 -0600

    models

---
+++ main_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    url = models.CharField(max_length=200)
+
+class Color(models.Model):
+    color = models.CharField(max_length=50)
+    def __str__(self):
+        return self.color
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    poop_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+    food_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class Poop(models.Model):
+    pass_date = models.DateField(auto_now=True, auto_now_add=False)
+    Bristol_Type = models.IntegerField()
+    note = models.CharField(max_length=250)
+    color = models.ForeignKey(Color,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class Food(models.Model):
+    ate_date = models.DateField(auto_now=True, auto_now_add=False)
+    note = models.CharField(max_length=250)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)

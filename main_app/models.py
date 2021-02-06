7c17aea1f7d357aef7499d426b1ba490bd18fedd
kennytrinh <kenny.trinh4444@gmail.com>
2021-02-06 15:19:33 -0600

    user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -8,11 +9,6 @@ class Color(models.Model):
     color = models.CharField(max_length=50)
     def __str__(self):
         return self.color
-
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=100)
-
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
@@ -27,7 +23,10 @@ class Poop(models.Model):
     color = models.ForeignKey(Color,on_delete=models.CASCADE)
 
 class Food(models.Model):
+    name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ate_date = models.DateField(auto_now=True, auto_now_add=False)
     note = models.CharField(max_length=250, null=True)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name

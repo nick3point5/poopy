cad9725a521f795150341c09f2d036b1fbca1f9f
kennytrinh <kenny.trinh4444@gmail.com>
2021-02-06 15:01:25 -0600

    profile added

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-""" class Image(models.Model):
-    url = models.CharField(max_length=200)
+class Image(models.Model):
+    url = models.CharField(max_length=200, null=True)
 
 class Color(models.Model):
     color = models.CharField(max_length=50)
@@ -12,19 +12,22 @@ class Color(models.Model):
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=100)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    poop_id = models.ForeignKey(Image, on_delete=models.CASCADE)
-    food_id = models.ForeignKey(Image, on_delete=models.CASCADE)
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
 class Poop(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     pass_date = models.DateField(auto_now=True, auto_now_add=False)
-    Bristol_Type = models.IntegerField()
-    note = models.CharField(max_length=250)
+    Bristol_Type = models.IntegerField(default=3)
+    note = models.CharField(max_length=250, null=True)
     color = models.ForeignKey(Color,on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    color = models.ForeignKey(Color,on_delete=models.CASCADE)
 
 class Food(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     ate_date = models.DateField(auto_now=True, auto_now_add=False)
-    note = models.CharField(max_length=250)
+    note = models.CharField(max_length=250, null=True)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
- """
